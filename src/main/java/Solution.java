3a54dc391c8778b207229cfcf69bca1e17aa53eb
Abreham Asfaw <abresh567@gmail.com>
2023-10-10 13:00:47 -0400

    add first three method and distinct gender method

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -6,6 +6,8 @@ public class Solution {
         System.out.println(malesOnly(Person.persons()));
         names(Person.persons()).forEach(System.out::println);
         sortedByIncomeDes().forEach(System.out::println);
+        distinctGenders().forEach(System.out::println);
+        firstThreePeople().forEach(System.out::println);
     }
 
     //challenge 1
@@ -26,10 +28,30 @@ public class Solution {
         return names;
     }
 
+    //challenge 3
     static List<Person> sortedByIncomeDes(){
         List<Person> sortedList = Person.persons().stream()
                 .sorted(Comparator.comparing(Person::getIncome).reversed())
                 .toList();
         return sortedList;
     }
+
+    //challenge 4
+    static List<Person.Gender> distinctGenders(){
+        List<Person.Gender> genders = Person.persons()
+                .stream()
+                .map(Person::getGender)
+                .distinct()
+                .toList();
+        return genders;
+    }
+    //challenge 5
+    static List<Person> firstThreePeople(){
+        List<Person> top3 = Person.persons()
+                .stream()
+                .limit(3)
+                .toList();
+        return top3;
+    }
+
 }
