b303cd2f1b53d281e97c31401eb13402eec7ea9d
Abreham Asfaw <abresh567@gmail.com>
2023-10-10 12:37:26 -0400

    add names method

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,8 +3,10 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(malesOnly(Person.persons()));
+        names(Person.persons()).forEach(System.out::println);
     }
 
+    //challenge 1
     static List<String> malesOnly(List<Person> people){
         people = Person.persons();
         List<String> males = people.stream()
@@ -12,5 +14,13 @@ public class Solution {
                 .map(Person::getName)
                 .toList();
         return males;
+    }
+
+    //challenge 2
+    static List<String> names(List<Person> people){
+        List<String> names = people.stream()
+                .map(Person::getName)
+                .toList();
+        return names;
     }
 }
